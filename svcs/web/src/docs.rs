a90ef865fe3b5445deb74dcd3902e355c841e71e
Michael <michael.ahli99@gmail.com>
2026-06-07 23:00:13 +0700

    chore: align license metadata with MIT

--- svcs/web/src/docs.rs
+++ svcs/web/src/docs.rs
@@ -17,8 +17,8 @@ use utoipa::OpenApi;
             url = "https://github.com/klyster/klyster"
         ),
         license(
-            name = "Apache-2.0",
-            url = "https://www.apache.org/licenses/LICENSE-2.0"
+            name = "MIT",
+            url = "https://opensource.org/licenses/MIT"
         )
     ),
     paths(
